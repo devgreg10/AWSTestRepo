4673db8c4cb0c6579eba76a9e6194817e58eb42c
David Kearfott <davidkearfott@gmail.com>
2024-08-21 09:22:34 -0400

    deploy lambda to create ec2 key pairs

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import aws_cdk as cdk
 from iac_code.first_tee_decision_support_stack import FirstTeeDecisionSupportStack
 from iac_code.ft_appflow_s3_to_s3_ingestion import FtAppflowS3ToS3IngestionStack
 from iac_code.ft_data_warehouse_aurora import FtDataWarehouseAuroraStack
+from iac_code.ft_ec2_key_pair_lambda import FtEc2KeyPairLambda
 
 from dotenv import load_dotenv
 
@@ -16,7 +17,8 @@ app = cdk.App()
 
 # FirstTeeDecisionSupportStack(app, "FirstTee-CDK-{0}".format(env), env)
 # FtAppflowS3ToS3IngestionStack(app, "FirstTee-CDK-AppFlow-S3-To-S3-Ingestion-{0}".format(env), env)
-FtDataWarehouseAuroraStack(app, "FirstTee-CDK-Data-Warehouse-Aurora-{0}".format(env), env)
+# FtDataWarehouseAuroraStack(app, "FirstTee-CDK-Data-Warehouse-Aurora-{0}".format(env), env)
+FtEc2KeyPairLambda(app, "FirstTee-CDK-Ec2-Key-Pair-Lambda", env)
 
 cdk.Tags.of(app).add("Project", "First Tee Decision Support")
 cdk.Tags.of(app).add("Deployment", "CDK")
