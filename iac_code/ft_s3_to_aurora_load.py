f572761809bc63a4483a09e5e73e43ccdee55208
David Kearfott <davidkearfott@gmail.com>
2024-08-22 17:04:51 -0400

    creating lambda layer construct

--- iac_code/ft_s3_to_aurora_load.py
+++ iac_code/ft_s3_to_aurora_load.py
@@ -18,7 +18,7 @@ class FtS3ToAuroraLoadStack(Stack):
         # Define a Lambda Layer for the psycopg2 library
         psycopg2_layer = lambda_.LayerVersion(
             self, 'Psycopg2Layer',
-            code=lambda_.Code.from_asset('lambda_layers/lambda_layer_v1.zip'),
+            code=lambda_.Code.from_asset('lambda_layers/API_Layer'),
             compatible_runtimes=[lambda_.Runtime.PYTHON_3_8, lambda_.Runtime.PYTHON_3_9],
             description="A layer for psycopg2",
         )
@@ -27,85 +27,9 @@ class FtS3ToAuroraLoadStack(Stack):
         lambda_function = lambda_.Function(self, "LambdaJsonToAurora",
             runtime=lambda_.Runtime.PYTHON_3_8,
             function_name="ft-" + env + "-load-json-s3-to-aurora",
-            handler="lambda_function.lambda_handler",
             layers=[psycopg2_layer],
-            code=lambda_.Code.from_inline(
-                """
-                import json
-                import boto3
-                import psycopg2
-                import os
-                import logging
-
-                logger = logging.getLogger()
-                logger.setLevel(logging.INFO)
-
-                def lambda_handler(event, context):
-                    # Log the secret ARN
-                    secret_arn = os.environ['DB_SECRET_ARN']
-                    logger.info(f"Using Secret ARN: {secret_arn}")
-                    
-                    s3 = boto3.client('s3')
-                    bucket_name = os.environ['BUCKET_NAME']
-                    bucket_prefix = os.environ['BUCKET_PREFIX']
-                    num_files = int(os.environ['NUM_FILES'])
-                    
-                    # List the first N JSON files in the bucket
-                    response = s3.list_objects_v2(Bucket=bucket_name, MaxKeys=num_files, Prefix=bucket_prefix)  # Add prefix if needed
-                    files = response.get('Contents', [])
-                    
-                    if not files:
-                        return {'statusCode': 200, 'body': 'No files to process'}
-                    
-                    # Retrieve the secret
-                    client = boto3.client('secretsmanager')
-                    secret_response = client.get_secret_value(SecretId=secret_arn)
-                    secret = json.loads(secret_response['SecretString'])
-                    
-                    # Connect to Aurora PostgreSQL
-                    conn = psycopg2.connect(
-                        host=secret['host'],
-                        database=secret['dbname'],
-                        user=secret['username'],
-                        password=secret['password']
-                    )
-                    
-                    try:
-                        with conn.cursor() as cursor:
-                            for file in files:
-                                file_key = file['Key']
-                                
-                                # Get the JSON file from S3
-                                response = s3.get_object(Bucket=bucket_name, Key=file_key)
-                                json_data = json.loads(response['Body'].read())
-                                
-                                # Example: Insert JSON data into the table
-                                for record in json_data:
-                                    cursor.execute(
-                                        "INSERT INTO your_table_name (column1, column2, column3) VALUES (%s, %s, %s)",
-                                        (record['field1'], record['field2'], record['field3'])
-                                    )
-                                
-                                # Move the file to the "Complete" folder
-                                destination_key = f'Complete/{file_key.split("/")[-1]}'
-                                s3.copy_object(Bucket=bucket_name, CopySource={'Bucket': bucket_name, 'Key': file_key}, Key=destination_key)
-                                s3.delete_object(Bucket=bucket_name, Key=file_key)
-                                
-                            conn.commit()
-                    
-                    except Exception as e:
-                        conn.rollback()
-                        raise e
-                    
-                    finally:
-                        conn.close()
-                    
-                    return {
-                        'statusCode': 200,
-                        'body': json.dumps('Data inserted and files moved successfully')
-                    }
-                """
-            ),
+            code=lambda_.Code.from_asset('lambdas'),
+            handler='FTDecisionSupportLoadLayerS3ToAurora.lambda_function.lambda_handler',
             environment={
                 "DB_SECRET_ARN": secret_arn,
                 "BUCKET_NAME": bucket_name,
