6762ac4ecdb7af33173db59c57a92d0bf8980841
David Kearfott <davidkearfott@gmail.com>
2024-08-22 14:13:43 -0400

    initial creation of lambda for Load Layer

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from iac_code.ft_data_warehouse_aurora import FtDataWarehouseAuroraStack
 from iac_code.ft_ec2_key_pair_lambda import FtEc2KeyPairLambda
 from iac_code.ft_public_data_warehouse_aurora import FtPublicDataWarehouseAuroraStack
 from iac_code.ft_create_secret import FtCreateSecretsStack
+from iac_code.ft_s3_to_aurora_load import FtS3ToAuroraLoadStack
 
 from dotenv import load_dotenv
 
@@ -17,12 +18,26 @@ env = os.environ.get('env')
 
 app = cdk.App()
 
+if (env=='prod'):
+    load_dotenv(".env.prod")
+elif (env=='uat'):
+    load_dotenv(".env.uat")
+else:
+    load_dotenv(".env.dev")
+
 # FirstTeeDecisionSupportStack(app, "FirstTee-CDK-{0}".format(env), env)
 # FtAppflowS3ToS3IngestionStack(app, "FirstTee-CDK-AppFlow-S3-To-S3-Ingestion-{0}".format(env), env)
 # FtDataWarehouseAuroraStack(app, "FirstTee-CDK-Data-Warehouse-Aurora-{0}".format(env), env)
 # FtEc2KeyPairLambda(app, "FirstTee-CDK-Ec2-Key-Pair-Lambda", env)
 # FtPublicDataWarehouseAuroraStack(app, "FirstTee-CDK-Public-Data-Warehouse-Aurora-{0}".format(env), env)
-FtCreateSecretsStack(app, "AuroraSecretsStack")
+# FtCreateSecretsStack(app, "AuroraSecretsStack")
+FtS3ToAuroraLoadStack(app, "FtS3ToAuroraLoadStack",
+    env=env,
+    secret_arn=os.getenv('db_connection_secret_arn'),
+    bucket_name=os.getenv('load_s3_bucket_name'),
+    bucket_prefix=os.getenv('load_s3_bucket_prefix'),
+    num_files=5  # Process the first 5 files
+)
 
 cdk.Tags.of(app).add("Project", "First Tee Decision Support")
 cdk.Tags.of(app).add("Deployment", "CDK")
---
+++ iac_code/ft_s3_to_aurora_load.py
@@ -0,0 +1,115 @@
+from aws_cdk import (
+    aws_lambda as lambda_,
+    aws_iam as iam,
+    aws_s3 as s3,
+    Stack
+)
+
+from constructs import Construct
+
+class FtS3ToAuroraLoadStack(Stack):
+
+    def __init__(self, scope: Construct, id: str, env: str, secret_arn: str, bucket_name: str, bucket_prefix: str, num_files: int, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Define the S3 bucket where the JSON files are stored
+        bucket = s3.Bucket.from_bucket_name(self, "MyBucket", bucket_name)
+
+        # Define the Lambda function
+        lambda_function = lambda_.Function(self, "LambdaJsonToAurora",
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            function_name="ft-" + env + "-load-json-s3-to-aurora",
+            handler="lambda_function.lambda_handler",
+            code=lambda_.Code.from_inline(
+                """
+                import json
+                import boto3
+                import psycopg2
+                import os
+                import logging
+
+                logger = logging.getLogger()
+                logger.setLevel(logging.INFO)
+
+                def lambda_handler(event, context):
+                    # Log the secret ARN
+                    secret_arn = os.environ['DB_SECRET_ARN']
+                    logger.info(f"Using Secret ARN: {secret_arn}")
+                    
+                    s3 = boto3.client('s3')
+                    bucket_name = os.environ['BUCKET_NAME']
+                    bucket_prefix = os.environ['BUCKET_PREFIX']
+                    num_files = int(os.environ['NUM_FILES'])
+                    
+                    # List the first N JSON files in the bucket
+                    response = s3.list_objects_v2(Bucket=bucket_name, MaxKeys=num_files, Prefix=bucket_prefix)  # Add prefix if needed
+                    files = response.get('Contents', [])
+                    
+                    if not files:
+                        return {'statusCode': 200, 'body': 'No files to process'}
+                    
+                    # Retrieve the secret
+                    client = boto3.client('secretsmanager')
+                    secret_response = client.get_secret_value(SecretId=secret_arn)
+                    secret = json.loads(secret_response['SecretString'])
+                    
+                    # Connect to Aurora PostgreSQL
+                    conn = psycopg2.connect(
+                        host=secret['host'],
+                        database=secret['dbname'],
+                        user=secret['username'],
+                        password=secret['password']
+                    )
+                    
+                    try:
+                        with conn.cursor() as cursor:
+                            for file in files:
+                                file_key = file['Key']
+                                
+                                # Get the JSON file from S3
+                                response = s3.get_object(Bucket=bucket_name, Key=file_key)
+                                json_data = json.loads(response['Body'].read())
+                                
+                                # Example: Insert JSON data into the table
+                                for record in json_data:
+                                    cursor.execute(
+                                        "INSERT INTO your_table_name (column1, column2, column3) VALUES (%s, %s, %s)",
+                                        (record['field1'], record['field2'], record['field3'])
+                                    )
+                                
+                                # Move the file to the "Complete" folder
+                                destination_key = f'Complete/{file_key.split("/")[-1]}'
+                                s3.copy_object(Bucket=bucket_name, CopySource={'Bucket': bucket_name, 'Key': file_key}, Key=destination_key)
+                                s3.delete_object(Bucket=bucket_name, Key=file_key)
+                                
+                            conn.commit()
+                    
+                    except Exception as e:
+                        conn.rollback()
+                        raise e
+                    
+                    finally:
+                        conn.close()
+                    
+                    return {
+                        'statusCode': 200,
+                        'body': json.dumps('Data inserted and files moved successfully')
+                    }
+                """
+            ),
+            environment={
+                "DB_SECRET_ARN": secret_arn,
+                "BUCKET_NAME": bucket_name,
+                "BUCKET_PREFIX": bucket_prefix,
+                "NUM_FILES": str(num_files)  # Convert to string for Lambda environment variable
+            }
+        )
+
+        # Grant the Lambda function permissions to read from S3 and Secrets Manager
+        bucket.grant_read_write(lambda_function)
+        lambda_function.add_to_role_policy(
+            iam.PolicyStatement(
+                actions=["secretsmanager:GetSecretValue"],
+                resources=[secret_arn],
+            )
+        )
