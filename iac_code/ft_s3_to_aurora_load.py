a348e60a88a2fc41503f69b20d11be15620cc369
David Kearfott <davidkearfott@gmail.com>
2024-08-23 15:17:22 -0400

    deploying lambda to appropriate VPC

--- iac_code/ft_s3_to_aurora_load.py
+++ iac_code/ft_s3_to_aurora_load.py
@@ -2,15 +2,44 @@ from aws_cdk import (
     aws_lambda as lambda_,
     aws_iam as iam,
     aws_s3 as s3,
+    aws_ec2 as ec2,
     Stack
 )
 
 from constructs import Construct
 
+from dotenv import load_dotenv
+import os
+
 class FtS3ToAuroraLoadStack(Stack):
 
     def __init__(self, scope: Construct, id: str, env: str, secret_arn: str, bucket_name: str, bucket_prefix: str, num_files: int, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        BASEDIR = os.path.abspath(os.path.dirname(__file__))
+        if (env=='prod'):
+            load_dotenv(os.path.join(BASEDIR, "../.env.prod"))
+        else:
+            load_dotenv(os.path.join(BASEDIR,"../.env.non_prod"))
+
+        vpc_name = os.getenv('vpc_name')
+        vpc_id = os.getenv('vpc_id')
+        public_subnets = [ec2.Subnet.from_subnet_attributes(
+                            self, 
+                            "PublicSubnetA", 
+                            subnet_id=os.getenv('public_subnet_a'),
+                            availability_zone=os.getenv("public_subnet_a_az")
+                          )]
+        private_subnets = [ec2.Subnet.from_subnet_id(self, "PrivateSubnetA", os.getenv('private_subnet_a')),
+                           ec2.Subnet.from_subnet_id(self, "PrivateSubnetD", os.getenv('private_subnet_d'))]
+
+        # Get a reference to the existing VPC
+        datawarehouse_vpc = ec2.Vpc.from_vpc_attributes(
+            self,
+            "ExistingVPC",
+            vpc_id=vpc_id,
+            availability_zones=["us-east-1a"]
+        )
 
         # Define the S3 bucket where the JSON files are stored
         bucket = s3.Bucket.from_bucket_name(self, "MyBucket", bucket_name)
@@ -28,6 +57,10 @@ class FtS3ToAuroraLoadStack(Stack):
             runtime=lambda_.Runtime.PYTHON_3_8,
             function_name="ft-" + env + "-load-json-s3-to-aurora",
             layers=[psycopg2_layer],
+            vpc=datawarehouse_vpc,
+            vpc_subnets=ec2.SubnetSelection(
+                subnets=public_subnets
+            ),
             code=lambda_.Code.from_asset('lambdas/FTDecisionSupportLoadLayerS3ToAurora'),
             handler='lambda_function.lambda_handler',
             environment={
--- lambdas/FTDecisionSupportLoadLayerS3ToAurora/lambda_function.py
+++ lambdas/FTDecisionSupportLoadLayerS3ToAurora/lambda_function.py
@@ -12,81 +12,94 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
 
-
-    # Define the time zone
-    eastern = pytz.timezone('America/New_York')
-    
-    # Get the current time
-    now = datetime.now(eastern)
-    
-    # Format the timestamp
-    formatted_timestamp = now.strftime('%Y-%m-%dT%H:%M:%S%z')
-    
-    # Adjust the format to include the colon in the offset
-    formatted_timestamp = f"{formatted_timestamp[:-2]}:{formatted_timestamp[-2:]}"
-    
-    # Log the secret ARN
-    secret_arn = os.environ['DB_SECRET_ARN']
-    logger.info(f"Using Secret ARN: {secret_arn}")
-    
-    s3 = boto3.client('s3')
-    bucket_name = os.environ['BUCKET_NAME']
-    bucket_prefix = os.environ['BUCKET_PREFIX']
-    num_files = int(os.environ['NUM_FILES'])
-    
-    # List the first N JSON files in the bucket
-    response = s3.list_objects_v2(Bucket=bucket_name, MaxKeys=num_files, Prefix=bucket_prefix)  # Add prefix if needed
-    files = response.get('Contents', [])
-    
-    if not files:
-        return {'statusCode': 200, 'body': 'No files to process'}
-    
-    # Retrieve the secret
-    client = boto3.client('secretsmanager')
-    secret_response = client.get_secret_value(SecretId=secret_arn)
-    secret = json.loads(secret_response['SecretString'])
-    
-    # Connect to Aurora PostgreSQL
-    conn = psycopg2.connect(
-        host=secret['host'],
-        database=secret['dbname'],
-        user=secret['username'],
-        password=secret['password']
-    )
-    
     try:
-        with conn.cursor() as cursor:
-            for file in files:
-                file_key = file['Key']
-                
-                # Get the JSON file from S3
-                response = s3.get_object(Bucket=bucket_name, Key=file_key)
-                json_data = json.loads(response['Body'].read())
-                
-                # Example: Insert JSON data into the table
-                for record in json_data:
-                    cursor.execute(
-                        "INSERT INTO ft_ds_raw.sf_contact_phase_zero_point_five " \
-                        "(snapshot_date, id, otherpostalcode, chapter_affiliation__c, chapterid_contact__c, casesafeid__c, contact_type__c, age__c, ethnicity__c, gender__c, grade__c, participation_status__c) " \
-                        "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                        (formatted_timestamp, record['Id'], record['OtherPostalCode'], record['Chapter_Affiliation__c'], record['ChapterID_CONTACT__c'], record['CASESAFEID__c'], record['Contact_Type__c'], record['Age__c'], record['Ethnicity__c'], record['Gender__c'], record['Grade__c'], record['Participation_Status__c'])
-                    )
-                
-                # Move the file to the "Complete" folder
-                destination_key = f'Complete/{file_key.split("/")[-1]}'
-                s3.copy_object(Bucket=bucket_name, CopySource={'Bucket': bucket_name, 'Key': file_key}, Key=destination_key)
-                s3.delete_object(Bucket=bucket_name, Key=file_key)
-                
-            conn.commit()
-    
+
+        # Define the time zone
+        eastern = pytz.timezone('America/New_York')
+        
+        # Get the current time
+        now = datetime.now(eastern)
+        
+        # Format the timestamp
+        formatted_timestamp = now.strftime('%Y-%m-%dT%H:%M:%S%z')
+        
+        # Adjust the format to include the colon in the offset
+        formatted_timestamp = f"{formatted_timestamp[:-2]}:{formatted_timestamp[-2:]}"
+        
+        # Log the secret ARN
+        secret_arn = os.environ['DB_SECRET_ARN']
+        logger.info(f"Using Secret ARN: {secret_arn}")
+        
+        s3 = boto3.client('s3')
+        bucket_name = os.environ['BUCKET_NAME']
+        bucket_prefix = os.environ['BUCKET_PREFIX']
+        num_files = int(os.environ['NUM_FILES'])
+        
+        logging.info("Bucket Name: " + bucket_name)
+        logging.info("Bucket Prefix: " + bucket_prefix)
+        
+        # List the first N JSON files in the bucket
+        response = s3.list_objects_v2(Bucket=bucket_name, MaxKeys=num_files, Prefix=bucket_prefix)  # Add prefix if needed
+        logging.info("Have response from s3.listobjects")
+        files = response.get('Contents', [])
+        logging.info("Have files")
+        
+        if not files:
+            return {'statusCode': 200, 'body': 'No files to process'}
+        
+        # Retrieve the secret
+        client = boto3.client('secretsmanager')
+        logging.info("1")
+        secret_response = client.get_secret_value(SecretId=secret_arn)
+        logging.info("2")
+        secret = json.loads(secret_response['SecretString'])
+        logging.info("3")
+
+        # Connect to Aurora PostgreSQL
+        conn = psycopg2.connect(
+            host=secret['host'],
+            database=secret['dbname'],
+            user=secret['username'],
+            password=secret['password']
+        )
+        
+        try:
+            with conn.cursor() as cursor:
+                for file in files:
+                    file_key = file['Key']
+                    
+                    # Get the JSON file from S3
+                    response = s3.get_object(Bucket=bucket_name, Key=file_key)
+                    json_data = json.loads(response['Body'].read())
+                    
+                    # Example: Insert JSON data into the table
+                    for record in json_data:
+                        cursor.execute(
+                            "INSERT INTO ft_ds_raw.sf_contact " \
+                            "(snapshot_date, id, otherpostalcode, chapter_affiliation__c, chapterid_contact__c, casesafeid__c, contact_type__c, age__c, ethnicity__c, gender__c, grade__c, participation_status__c) " \
+                            "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                            (formatted_timestamp, record['Id'], record['OtherPostalCode'], record['Chapter_Affiliation__c'], record['ChapterID_CONTACT__c'], record['CASESAFEID__c'], record['Contact_Type__c'], record['Age__c'], record['Ethnicity__c'], record['Gender__c'], record['Grade__c'], record['Participation_Status__c'])
+                        )
+                    
+                    # Move the file to the "Complete" folder
+                    destination_key = f'Complete/{file_key.split("/")[-1]}'
+                    s3.copy_object(Bucket=bucket_name, CopySource={'Bucket': bucket_name, 'Key': file_key}, Key=destination_key)
+                    s3.delete_object(Bucket=bucket_name, Key=file_key)
+                    
+                conn.commit()
+        
+        except Exception as e:
+            logger.error(f"DB Error occurred: {e}")
+            conn.rollback()
+            raise e
+        
+        finally:
+            conn.close()
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Data inserted and files moved successfully')
+        }
     except Exception as e:
-        conn.rollback()
+        logger.error(f"Error occurred: {e}")
         raise e
-    
-    finally:
-        conn.close()
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Data inserted and files moved successfully')
-    }
