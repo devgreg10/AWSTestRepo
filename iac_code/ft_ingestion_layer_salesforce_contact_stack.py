efb218b79f85a43eb605fccd5595d1003af3b99b
David Kearfott <davidkearfott@gmail.com>
2024-09-04 11:50:06 -0400

    updated out of whack parenthesis

--- iac_code/ft_ingestion_layer_salesforce_contact_stack.py
+++ iac_code/ft_ingestion_layer_salesforce_contact_stack.py
@@ -67,7 +67,12 @@ class FtSalesforceContactIngestionLayerStack(Stack):
                 destination_connector_properties=appflow.CfnFlow.DestinationConnectorPropertiesProperty(
                     s3=appflow.CfnFlow.S3DestinationPropertiesProperty(
                         bucket_name=destination_bucket.bucket_name,
-                        bucket_prefix="f({datalake_bucket_folder}ingress)",
+                        bucket_prefix=f"{datalake_bucket_folder}ingress",
+                        s3_output_format_config=appflow.CfnFlow.S3OutputFormatConfigProperty(
+                            aggregation_config=appflow.CfnFlow.AggregationConfigProperty(
+                                aggregation_type="None",
+                                target_file_size=128
+                            ),
                             file_type="JSON",
                             prefix_config=appflow.CfnFlow.PrefixConfigProperty(
                                 prefix_format="MINUTE", # Determines the level of granularity for the date and time that's included in the prefix.
