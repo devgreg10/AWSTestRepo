e37425402d2f470d43eca659ab1d8fd0e4a55dc5
David Kearfott <davidkearfott@gmail.com>
2024-10-01 13:45:54 -0400

    adding load and transform layer back

--- app.py
+++ app.py
@@ -69,37 +69,37 @@ Layer 5 - LOAD LAYER
 '''
 
 #Salesforce
-# load_layer_salesforce_stack = FtLoadLayerSalesforceStack(
-#     app, 
-#     id = f"ft-{env}-load-layer-salesforce-stack",
-#     env = env,
-#     region=os.getenv('default_region'),
-#     email_addresses_to_alert_on_error=os.getenv('email_addresses_to_alert_on_error'),
-#     concurrent_lambdas = os.getenv('load_layer_salesforce_concurrent_lambdas'),
-#     commit_batch_size = os.getenv('load_layer_commit_batch_size'),
-#     bootstrap_stack = decision_support_bootstrap_stack,
-#     storage_stack = decision_support_persistent_storage_stack,
-#     ds_core_stack = decision_support_base_stack,
-#     ingestion_layer_stack=ingestion_layer_salesforce_stack
-# )
+load_layer_salesforce_stack = FtLoadLayerSalesforceStack(
+    app, 
+    id = f"ft-{env}-load-layer-salesforce-stack",
+    env = env,
+    region=os.getenv('default_region'),
+    email_addresses_to_alert_on_error=os.getenv('email_addresses_to_alert_on_error'),
+    concurrent_lambdas = os.getenv('load_layer_salesforce_concurrent_lambdas'),
+    commit_batch_size = os.getenv('load_layer_commit_batch_size'),
+    bootstrap_stack = decision_support_bootstrap_stack,
+    storage_stack = decision_support_persistent_storage_stack,
+    ds_core_stack = decision_support_base_stack,
+    ingestion_layer_stack=ingestion_layer_salesforce_stack
+)
 
 '''
 Layer 6 - TRANSFORM LAYER
 '''
 
 #Salesforce
-# transform_layer_salesforce_stack = FtTransformLayerSalesforceStack(
-#     app, 
-#     id = f"ft-{env}-transform-layer-salesforce-stack",
-#     env = env,
-#     region=os.getenv('default_region'),
-#     email_addresses_to_alert_on_error=os.getenv('email_addresses_to_alert_on_error'),
-#     bootstrap_stack = decision_support_bootstrap_stack,
-#     storage_stack = decision_support_persistent_storage_stack,
-#     ds_core_stack = decision_support_base_stack,
-#     ingestion_layer_stack=ingestion_layer_salesforce_stack,
-#     load_layer_stack=load_layer_salesforce_stack
-# )
+transform_layer_salesforce_stack = FtTransformLayerSalesforceStack(
+    app, 
+    id = f"ft-{env}-transform-layer-salesforce-stack",
+    env = env,
+    region=os.getenv('default_region'),
+    email_addresses_to_alert_on_error=os.getenv('email_addresses_to_alert_on_error'),
+    bootstrap_stack = decision_support_bootstrap_stack,
+    storage_stack = decision_support_persistent_storage_stack,
+    ds_core_stack = decision_support_base_stack,
+    ingestion_layer_stack=ingestion_layer_salesforce_stack,
+    load_layer_stack=load_layer_salesforce_stack
+)
 
 '''
 TAGS
