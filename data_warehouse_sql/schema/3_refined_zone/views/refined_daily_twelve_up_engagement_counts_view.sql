4f6d0b725f5e2b245b0bab8f3cb2af40e6b1455a
Harry Schuster <hschuster@captechconsulting.com>
2024-10-14 21:39:30 +0000

    Squashed commit of the following
    commit 9376e69c 
    Author: Harry Schuster <hschuster@captechconsulting.com> 
    Date: Mon Oct 14 2024 17:26:08 GMT-0400 (Eastern Daylight Time) 
    
        renamed tenure counts daily view file to be consistent and added the underserved areas counts daily view
    
    
    commit 28e98940 
    Author: Harry Schuster <hschuster@captechconsulting.com> 
    Date: Mon Oct 14 2024 17:21:37 GMT-0400 (Eastern Daylight Time) 
    
        added 12+ engagement counts daily view
    
    
    commit 7444b977 
    Author: Harry Schuster <hschuster@captechconsulting.com> 
    Date: Mon Oct 14 2024 17:14:05 GMT-0400 (Eastern Daylight Time) 
    
        added teen retention daily view
    
    
    commit b14759cf 
    Author: Harry Schuster <hschuster@captechconsulting.com> 
    Date: Mon Oct 14 2024 17:11:12 GMT-0400 (Eastern Daylight Time) 
    
        added daily retention view
    
    
    commit 9cbafefd 
    Author: Harry Schuster <hschuster@captechconsulting.com> 
    Date: Mon Oct 14 2024 17:07:35 GMT-0400 (Eastern Daylight Time) 
    
        added female percentage daily metric
    
    
    commit ca253af0 
    Author: Harry Schuster <hschuster@captechconsulting.com> 
    Date: Mon Oct 14 2024 17:01:29 GMT-0400 (Eastern Daylight Time) 
    
        renamed the active partiicpants and then also added the ethnic diversity daily metric
    
    
    commit 338af1da 
    Author: Harry Schuster <hschuster@captechconsulting.com> 
    Date: Mon Oct 14 2024 16:56:14 GMT-0400 (Eastern Daylight Time) 
    
        added second daily view featuring active participant counts
    
    
    commit b4108c17 
    Author: Harry Schuster <hschuster@captechconsulting.com> 
    Date: Mon Oct 14 2024 16:51:59 GMT-0400 (Eastern Daylight Time) 
    
        added first daily historical metric view for tenure counts

---
+++ data_warehouse_sql/schema/3_refined_zone/views/refined_daily_twelve_up_engagement_counts_view.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE VIEW ft_ds_refined.metric_historical_daily_twelve_up_engagement_counts_view AS
+    SELECT
+        dates.metric_calc_date,
+        info.chapter_id,
+        info.twelve_up_engagement_counts,
+        info.twelve_up_total_counts,
+        info.twelve_up_engagement_percentage
+    FROM (
+        SELECT
+            MAX(metric_calc_date) AS latest_calc_timestamp,
+            chapter_id,
+            CAST(metric_calc_date AS DATE) as metric_calc_date
+        FROM
+        	ft_ds_refined.metric_historical_twelve_up_engagement_counts
+        GROUP BY
+            CAST(metric_calc_date AS DATE),
+            chapter_id
+    ) dates
+    JOIN
+        ft_ds_refined.metric_historical_twelve_up_engagement_counts info
+    ON
+        dates.latest_calc_timestamp = info.metric_calc_date
+        AND dates.chapter_id = info.chapter_id
+;
