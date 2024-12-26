2a119f816cb6c4b9d512c9d16ef18a57119e0cd9
Harry Schuster <hschuster@captechconsulting.com>
2024-12-26 14:29:55 -0500

    changes metric calculation to adjust which records are listed as the eoy indicated records for this given year

--- data_warehouse_sql/schema/3_refined_zone/functions/calculate_historical_active_participant_counts.sql
+++ data_warehouse_sql/schema/3_refined_zone/functions/calculate_historical_active_participant_counts.sql
@@ -3,11 +3,20 @@ RETURNS void
 LANGUAGE plpgsql
 AS $$
 BEGIN
+    UPDATE
+        ft_ds_refined.metric_historical_active_participant_counts
+    SET
+        eoy_indicator = NULL
+    WHERE
+        eoy_indicator = CAST(EXTRACT(YEAR FROM NOW()) AS TEXT)
+    ;
+
     INSERT INTO ft_ds_refined.metric_historical_active_participant_counts
     SELECT
         NOW() as metric_calc_date,
         chapter_id,
-        COUNT(contact_id_18) AS participant_count
+        COUNT(contact_id_18) AS participant_count,
+        CAST(EXTRACT(YEAR FROM NOW()) AS TEXT) AS eoy_indicator
     FROM ft_ds_refined.active_participants_view
     GROUP BY
     chapter_id
