ea5645dee2c2a8a0b820bbfe55f185eba1c120fc
Harry Schuster <hschuster@captechconsulting.com>
2024-12-26 14:12:44 -0500

    handles addition of the eoy_indicator field and migration of data to the new version of that table, with the addition of the historical metrics calculated for years past

--- data_warehouse_sql/schema/3_refined_zone/tables/refined_historical_active_participant_counts_table.sql
+++ data_warehouse_sql/schema/3_refined_zone/tables/refined_historical_active_participant_counts_table.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS ft_ds_refined.metric_historical_active_participant_co
     PRIMARY KEY(metric_calc_date, chapter_id),
     metric_calc_date TIMESTAMPTZ,
     chapter_id CHAR(18),
-    participant_count INTEGER
+    participant_count INTEGER,
+    eoy_indicator VARCHAR(20)
 );
