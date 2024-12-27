ced3a2a4dd20222796d5bd9f82b202477f08a0c1
Harry Schuster <hschuster@captechconsulting.com>
2024-12-27 15:35:23 -0500

    reformatted retention metric table and added historical data to table along with values for eoy_indicator (steps 1 and 2)

--- data_warehouse_sql/schema/3_refined_zone/tables/refined_historical_retention_percentage_table.sql
+++ data_warehouse_sql/schema/3_refined_zone/tables/refined_historical_retention_percentage_table.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS ft_ds_refined.metric_historical_retention_percentage 
     PRIMARY KEY(metric_calc_date, chapter_id),
     metric_calc_date TIMESTAMPTZ,
     chapter_id CHAR(18),
-    retention_percentage NUMERIC(5,2)
+    retention_percentage NUMERIC(5,2),
+    eoy_indicator VARCHAR(20)
 );
