35f4926d3e4e76b550b6aac5b2b3e27c4fcfb9e8
Harry Schuster <hschuster@captechconsulting.com>
2025-01-14 11:49:58 -0500

    changed active participants counts column name since that was previously forgotten, and added ethnic div percentage to CPA report view

--- data_warehouse_sql/schema/3_refined_zone/views/refined_report_cpa_first_tee_programs_view.sql
+++ data_warehouse_sql/schema/3_refined_zone/views/refined_report_cpa_first_tee_programs_view.sql
@@ -1,9 +1,10 @@
 CREATE OR REPLACE VIEW ft_ds_refined.report_cpa_first_tee_programs AS
 SELECT
     chapter_info.account_name as chapter_name,
-    averages_info.first_tee_program_particpants,
+    averages_info.first_tee_program_participants,
     averages_info.percent_annual_retention,
     averages_info.percent_annual_teen_retention,
+    averages_info.percent_diverse,
     averages_info.eoy_indicator
 FROM
 --this subquery contains the information about the EOY information for each of the chapters, for each year
@@ -12,9 +13,10 @@ FROM
     --the base table everything else is LEFT JOINED to is the active participant counts since it is the most basic metric
     SELECT
         counts.chapter_id,
-        counts.participant_count AS first_tee_program_particpants,
+        counts.participant_count AS first_tee_program_participants,
         retention.retention_percentage AS percent_annual_retention,
         teen_retention.teen_retention_percentage AS percent_annual_teen_retention,
+        ethnic_diversity.ethnic_diversity_percentage AS percent_diverse,
         counts.eoy_indicator
     FROM ft_ds_refined.metric_historical_active_participant_counts counts
     LEFT JOIN ft_ds_refined.metric_historical_retention_percentage retention
@@ -23,6 +25,9 @@ FROM
     LEFT JOIN ft_ds_refined.metric_historical_teen_retention_percentage teen_retention
         ON counts.chapter_id = teen_retention.chapter_id
         AND counts.eoy_indicator = teen_retention.eoy_indicator
+    LEFT JOIN ft_ds_refined.metric_historical_ethnic_diversity_percentage ethnic_diversity
+        ON counts.chapter_id = ethnic_diversity.chapter_id
+        AND counts.eoy_indicator = ethnic_diversity.eoy_indicator
     --this part will be the peer group averages of the current year. It needs to include all the metrics that are listed above
     UNION
     (
@@ -38,6 +43,7 @@ FROM
             peer_group_averages.participant_count AS first_tee_program_particpants,
             peer_group_averages.retention_percentage AS percent_annual_retention,
             peer_group_averages.teen_retention_percentage AS percent_annual_teen_retention,
+            peer_group_averages.ethnic_diversity_percentage AS percent_diverse,
             'Curr Yr Peer Grp Avg' AS eoy_indicator
         FROM
         (
@@ -45,7 +51,8 @@ FROM
                 peer_group_map.peer_group_level,
                 AVG(active_participants_counts_info.participant_count) AS participant_count,
                 AVG(retention_percentage_info.retention_percentage) AS retention_percentage,
-                AVG(teen_retention_percentage_info.teen_retention_percentage) AS teen_retention_percentage
+                AVG(teen_retention_percentage_info.teen_retention_percentage) AS teen_retention_percentage,
+                AVG(ethnic_diversity_percentage_info.ethnic_diversity_percentage) AS ethnic_diversity_percentage
             FROM
             peer_group_map
             LEFT JOIN
@@ -75,6 +82,15 @@ FROM
                 WHERE eoy_indicator = CAST(EXTRACT(YEAR FROM NOW()) AS TEXT)
             ) teen_retention_percentage_info
                 ON peer_group_map.account_id = teen_retention_percentage_info.chapter_id
+            LEFT JOIN
+            (
+                SELECT
+                    chapter_id,
+                    ethnic_diversity_percentage
+                FROM ft_ds_refined.metric_historical_ethnic_diversity_percentage
+                WHERE eoy_indicator = CAST(EXTRACT(YEAR FROM NOW()) AS TEXT)
+            ) ethnic_diversity_percentage_info
+                ON peer_group_map.account_id = ethnic_diversity_percentage_info.chapter_id
             GROUP BY peer_group_map.peer_group_level
         ) peer_group_averages
         JOIN 
