b9c5205dd3175e7c1e4700037d7f78b4df3b9c7f
Harry Schuster <hschuster@captechconsulting.com>
2025-01-14 15:52:56 -0500

    added a last line to the calculation function to enable the eoy_indicator field

--- data_warehouse_sql/schema/3_refined_zone/functions/calculate_historical_female_percentage.sql
+++ data_warehouse_sql/schema/3_refined_zone/functions/calculate_historical_female_percentage.sql
@@ -17,7 +17,8 @@ BEGIN
         chapter_id,
         (COUNT(CASE WHEN gender ='Female' THEN 1 END) * 1.0)/
         NULLIF(COUNT(CASE WHEN gender <> 'Prefer not to respond' THEN 1 END), 0)
-        AS female_percentage
+        AS female_percentage,
+        CAST(EXTRACT(YEAR FROM NOW()) AS TEXT) AS eoy_indicator
     FROM ft_ds_refined.active_participants_view
     GROUP BY
     chapter_id
