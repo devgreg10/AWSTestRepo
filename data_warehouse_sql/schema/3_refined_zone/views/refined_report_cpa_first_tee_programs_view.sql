6634221e48b5e3f974834fcb0fe09f247e3b7cf2
Harry Schuster <hschuster@captechconsulting.com>
2025-01-14 17:03:39 -0500

    added the non-male metric to the CPA report view and set the metric calculation on the schedule

--- data_warehouse_sql/schema/3_refined_zone/views/refined_report_cpa_first_tee_programs_view.sql
+++ data_warehouse_sql/schema/3_refined_zone/views/refined_report_cpa_first_tee_programs_view.sql
@@ -6,6 +6,7 @@ SELECT
     averages_info.percent_annual_teen_retention,
     averages_info.percent_diverse,
     averages_info.percent_female,
+    averages_info.percent_non_male,
     averages_info.eoy_indicator
 FROM
 --this subquery contains the information about the EOY information for each of the chapters, for each year
@@ -19,6 +20,7 @@ FROM
         teen_retention.teen_retention_percentage AS percent_annual_teen_retention,
         ethnic_diversity.ethnic_diversity_percentage AS percent_diverse,
         female.female_percentage AS percent_female,
+        non_male.non_male_percentage AS percent_non_male,
         counts.eoy_indicator
     FROM ft_ds_refined.metric_historical_active_participant_counts counts
     LEFT JOIN ft_ds_refined.metric_historical_retention_percentage retention
@@ -33,6 +35,9 @@ FROM
     LEFT JOIN ft_ds_refined.metric_historical_female_percentage female
         ON counts.chapter_id = female.chapter_id
         AND counts.eoy_indicator = female.eoy_indicator
+    LEFT JOIN ft_ds_refined.metric_historical_non_male_percentage non_male
+        ON counts.chapter_id = non_male.chapter_id
+        AND counts.eoy_indicator = non_male.eoy_indicator
     --this part will be the peer group averages of the current year. It needs to include all the metrics that are listed above
     UNION
     (
@@ -50,6 +55,7 @@ FROM
             peer_group_averages.teen_retention_percentage AS percent_annual_teen_retention,
             peer_group_averages.ethnic_diversity_percentage AS percent_diverse,
             peer_group_averages.female_percentage AS percent_female,
+            peer_group_averages.non_male_percentage AS percent_non_male,
             'Curr Yr Peer Grp Avg' AS eoy_indicator
         FROM
         (
@@ -59,7 +65,8 @@ FROM
                 AVG(retention_percentage_info.retention_percentage) AS retention_percentage,
                 AVG(teen_retention_percentage_info.teen_retention_percentage) AS teen_retention_percentage,
                 AVG(ethnic_diversity_percentage_info.ethnic_diversity_percentage) AS ethnic_diversity_percentage,
-                AVG(female_percentage_info.female_percentage) AS female_percentage
+                AVG(female_percentage_info.female_percentage) AS female_percentage,
+                AVG(non_male_percentage_info.non_male_percentage) AS non_male_percentage
             FROM
             peer_group_map
             LEFT JOIN
@@ -107,6 +114,15 @@ FROM
                 WHERE eoy_indicator = CAST(EXTRACT(YEAR FROM NOW()) AS TEXT)
             ) female_percentage_info
                 ON peer_group_map.account_id = female_percentage_info.chapter_id
+            LEFT JOIN
+            (
+                SELECT
+                    chapter_id,
+                    non_male_percentage
+                FROM ft_ds_refined.metric_historical_non_male_percentage
+                WHERE eoy_indicator = CAST(EXTRACT(YEAR FROM NOW()) AS TEXT)
+            ) non_male_percentage_info
+                ON peer_group_map.account_id = non_male_percentage_info.chapter_id
             GROUP BY peer_group_map.peer_group_level
         ) peer_group_averages
         JOIN 
