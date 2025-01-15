c2c93d14ad4c25f1dc090091c6bdc7fa1f100a01
Harry Schuster <hschuster@captechconsulting.com>
2025-01-15 15:36:11 -0500

    forgot a filter

--- data_warehouse_sql/schema/3_refined_zone/scripts/populate_historical_active_participant_counts_by_age.sql
+++ data_warehouse_sql/schema/3_refined_zone/scripts/populate_historical_active_participant_counts_by_age.sql
@@ -7,6 +7,8 @@ SELECT
     year::TEXT AS eoy_indicator
 FROM ft_ds_refined.current_and_historical_participants_view 
 WHERE year != CAST(EXTRACT(YEAR FROM NOW()) AS INTEGER)
+--needs this filter since the international chapters do not have birthdates, so they will have NULL calculated ages. The presence of NULL calculated ages results in PK constraint errors
+AND calculated_age IS NOT NULL
 GROUP BY year, chapter_id, calculated_age
 ON CONFLICT (metric_calc_date, chapter_id, age) DO UPDATE SET
         participant_count = EXCLUDED.participant_count,
