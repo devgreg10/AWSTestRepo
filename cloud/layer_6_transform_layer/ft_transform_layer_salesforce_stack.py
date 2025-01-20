fa6eaa574e5a6684c8a58d0ec133ea5944f549b6
Harry Schuster <hschuster@captechconsulting.com>
2025-01-20 15:57:43 -0500

    added ace certified metric calculation to schedule

--- cloud/layer_6_transform_layer/ft_transform_layer_salesforce_stack.py
+++ cloud/layer_6_transform_layer/ft_transform_layer_salesforce_stack.py
@@ -163,7 +163,8 @@ class FtTransformLayerSalesforceStack(Stack):
             {"db_function": "calculate_historical_non_male_percentage", "short_name": "non-male-percentage"},
             {"db_function": "calculate_historical_active_participant_counts_by_age", "short_name": "active-counts-by-age"},
             {"db_function": "calculate_historical_teen_percentage", "short_name": "teen-percentage"},
-            {"db_function": "calculate_historical_ACE_participant_counts", "short_name": "ACE-counts"}
+            {"db_function": "calculate_historical_ACE_participant_counts", "short_name": "ACE-counts"},
+            {"db_function": "calculate_historical_ACE_certified_participant_counts", "short_name": "ACE-certified-counts"}
         ]
 
         # Historical Metric Functions should be run once per day at 3:00am EST, no modulo
