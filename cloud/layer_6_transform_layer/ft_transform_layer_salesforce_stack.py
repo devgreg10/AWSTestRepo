0f093dfeb650d8b381f14993bdc1e0ed43f4b226
Harry Schuster <hschuster@captechconsulting.com>
2025-01-20 14:53:52 -0500

    added metric calculation for ACE counts to schedule

--- cloud/layer_6_transform_layer/ft_transform_layer_salesforce_stack.py
+++ cloud/layer_6_transform_layer/ft_transform_layer_salesforce_stack.py
@@ -162,7 +162,8 @@ class FtTransformLayerSalesforceStack(Stack):
             {"db_function": "calculate_historical_twelve_plus_retention_percentage", "short_name": "twelve-plus-retention-percentage"},
             {"db_function": "calculate_historical_non_male_percentage", "short_name": "non-male-percentage"},
             {"db_function": "calculate_historical_active_participant_counts_by_age", "short_name": "active-counts-by-age"},
-            {"db_function": "calculate_historical_teen_percentage", "short_name": "teen-percentage"}
+            {"db_function": "calculate_historical_teen_percentage", "short_name": "teen-percentage"},
+            {"db_function": "calculate_historical_ACE_participant_counts", "short_name": "ACE-counts"}
         ]
 
         # Historical Metric Functions should be run once per day at 3:00am EST, no modulo
