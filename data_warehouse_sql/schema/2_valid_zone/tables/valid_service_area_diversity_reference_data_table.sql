64700ab2424ef21af0ea682eab79c045a40c8af6
Harry Schuster <hschuster@captechconsulting.com>
2025-01-21 15:21:38 -0500

    get service area diversity reference data loaded into valid

---
+++ data_warehouse_sql/schema/2_valid_zone/tables/valid_service_area_diversity_reference_data_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS ft_ds_valid.service_area_diversity_reference_data (
+    PRIMARY KEY (chapter_id, upload_date),
+    chapter_id CHAR(18),
+    chapter_name VARCHAR(100),
+    asian_count INTEGER,
+    black_or_african_american_count INTEGER,
+    latino_or_hispanic_count INTEGER,
+    multi_racial_count INTEGER,
+    native_american_or_native_alaskan_count INTEGER,
+    pacific_islander_count INTEGER,
+    white_or_caucasian_count INTEGER,
+    youth_population_count INTEGER,
+    market_diversity_percentage NUMERIC(5,3),
+    upload_date DATE
+);
