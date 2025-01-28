0eb88a35e217172845632943879b1eacca3fc775
Harry Schuster <hschuster@captechconsulting.com>
2025-01-28 15:25:10 -0500

    changed precision of ethnic percentage metric to 3 instead of 2

--- data_warehouse_sql/schema/3_refined_zone/scripts/migrate_historical_ethnic_diversity_percentage.sql
+++ data_warehouse_sql/schema/3_refined_zone/scripts/migrate_historical_ethnic_diversity_percentage.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS ft_ds_refined.metric_historical_ethnic_diversity_perc
     PRIMARY KEY(metric_calc_date, chapter_id),
     metric_calc_date TIMESTAMPTZ,
     chapter_id CHAR(18),
-    ethnic_diversity_percentage NUMERIC(5,2),
+    ethnic_diversity_percentage NUMERIC(5,3),
     eoy_indicator VARCHAR(20)
 );
 
