1a515326902a48bbd648408fd7bcbb8e036a2adf
Spencer McIntosh <spencermcintosh@firsttee.org>
2025-05-28 14:32:37 +0000

    Edited calculate_historical_waitlist_not_selected_counts.sql

--- data_warehouse_sql/schema/3_refined_zone/functions/calculate_historical_waitlist_not_selected_counts.sql
+++ data_warehouse_sql/schema/3_refined_zone/functions/calculate_historical_waitlist_not_selected_counts.sql
@@ -1,3 +1,14 @@
+-- FUNCTION: ft_ds_admin.calculate_historical_waitlist_not_selected_counts()
+
+-- DROP FUNCTION IF EXISTS ft_ds_admin.calculate_historical_waitlist_not_selected_counts();
+
+CREATE OR REPLACE FUNCTION ft_ds_admin.calculate_historical_waitlist_not_selected_counts(
+	)
+    RETURNS void
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
 BEGIN
     UPDATE
         ft_ds_refined.metric_historical_waitlist_not_selected_counts
@@ -11,13 +22,20 @@ BEGIN
     SELECT
         NOW() as metric_calc_date,
         chapter_id,
-        COUNT(waitlist_id) AS waitlist_not_selected_count,
+        COUNT(contact_id_18) AS participant_count,
         CAST(EXTRACT(YEAR FROM NOW()) AS TEXT) AS eoy_indicator
-    FROM 
-        ft_ds_refined.waitlist_view
-    WHERE
-        status = 'Waitlist Pending'
+    FROM ft_ds_refined.waitlist_view
     GROUP BY
     chapter_id
     ;
 END;
+$BODY$;
+
+ALTER FUNCTION ft_ds_admin.calculate_historical_waitlist_not_selected_counts()
+    OWNER TO ft_prod_data_warehouse_master_user;
+
+GRANT EXECUTE ON FUNCTION ft_ds_admin.calculate_historical_waitlist_not_selected_counts() TO PUBLIC;
+
+GRANT EXECUTE ON FUNCTION ft_ds_admin.calculate_historical_waitlist_not_selected_counts() TO ft_prod_data_warehouse_master_user;
+
+GRANT EXECUTE ON FUNCTION ft_ds_admin.calculate_historical_waitlist_not_selected_counts() TO limited_writer_role;
